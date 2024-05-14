5911615ea509ec5d5b600a5df0b31f214789eeeb
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2024-05-14 14:12:52 +0000

    basic layout

---
+++ src/routes/(postviews)/+layout.svelte
@@ -0,0 +1,22 @@
+<div class="wrap">
+    <div class="inner">
+        <div class="sidebar">
+        </div>
+        <slot/>
+    </div>
+</div>
+
+<style lang="scss">
+    .wrap {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: start;
+        justify-content: center;
+    }
+
+    .inner {
+        width: 80%;
+        height: 100%;
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import '@fontsource-variable/raleway';
+</script>
+
+<div>
+    <slot/>
+</div>
+
+<style lang="scss">
+    :global(body) {
+        --bg: #1c1c1c;
+        --text: #f1f1f1;
+
+        font-family: 'Raleway Variable', sans-serif;
+        background: var(--bg);
+        color: var(--text);
+
+        margin: 0px;
+    }
+</style>
+
